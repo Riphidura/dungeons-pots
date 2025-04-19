f91ab3f050f916097f1d5dd98db1e7900516cf01
Riphidura <ilhanba1103@gmail.com>
2025-04-19 19:37:21 +0200

    Initial commit

---
+++ src/client/java/git/riphidura/dungeonspots/DungeonspotsClient.java
@@ -0,0 +1,10 @@
+package git.riphidura.dungeonspots;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DungeonspotsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
