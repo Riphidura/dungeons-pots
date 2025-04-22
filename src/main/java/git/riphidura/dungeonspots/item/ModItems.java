5b4c18005ffb6165d3bc2a220f69eb3788277de9
Riphidura <ilhanba1103@gmail.com>
2025-04-22 22:43:23 +0200

    Added sherd item to ingredients group

--- src/main/java/git/riphidura/dungeonspots/item/ModItems.java
+++ src/main/java/git/riphidura/dungeonspots/item/ModItems.java
@@ -3,7 +3,9 @@ package git.riphidura.dungeonspots.item;
 import java.util.function.Function;
 
 import git.riphidura.dungeonspots.DungeonsPots;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
@@ -28,5 +30,7 @@ public class ModItems {
     }
 
 	public static void initialize() {
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+		.register((itemGroup) -> itemGroup.add(ModItems.MY_SHERD));
 	}
 }
