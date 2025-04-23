fe48c68b6182db11dfaa3e6880e68114d1a7f4d1
Riphidura <ilhanba1103@gmail.com>
2025-04-23 12:00:42 +0200

    Set rarity and order in item group

--- src/main/java/git/riphidura/dungeonspots/item/ModItems.java
+++ src/main/java/git/riphidura/dungeonspots/item/ModItems.java
@@ -7,15 +7,19 @@ import git.riphidura.dungeonspots.DungeonsPots;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
 
 
 public class ModItems {
-	public static final Item MY_SHERD = register("my_sherd", Item::new, new Item.Settings().component(SherdsApiDataComponents.SHERD_PATTERN.get(), Identifier.of(DungeonsPots.MOD_ID, "my_sherd")));
+	public static final Item MY_SHERD = register("my_sherd", Item::new, new Item.Settings().
+			component(SherdsApiDataComponents.SHERD_PATTERN.get(), Identifier.of(DungeonsPots.MOD_ID, "my_sherd"))
+			.rarity(Rarity.UNCOMMON));
 
     public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
         // Create the item key.
@@ -32,6 +36,6 @@ public class ModItems {
 
 	public static void initialize() {
 		ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
-		.register((itemGroup) -> itemGroup.add(ModItems.MY_SHERD));
+		.register((itemGroup) -> itemGroup.addAfter(Items.SNORT_POTTERY_SHERD, ModItems.MY_SHERD));
 	}
 }
